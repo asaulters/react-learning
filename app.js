9adfd264d076e53df28af3532efff7d3daa363b4
asaulters <71015650+asaulters@users.noreply.github.com>
2022-01-12 21:14:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,70 @@
-function Header() {
+const players = [
+    {
+        name: "Guil",
+        score: 50
+      },
+      {
+        name: "Treasure",
+        score: 85
+      },
+      {
+        name: "Ashley",
+        score: 95
+      },
+      {
+        name: "James",
+        score: 80
+      }
+]
+
+function Header(props) {
     return  (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Adam
+                {props.name}
             </span>
+            <Counter score={props.score}/>
+        </div>
+    )
+}
 
-            <div className="counter">
+const Counter = (props) => {
+    return (
+        <div className="counter">
                 <button className="counter-action decrement"> -</button>
-                <span className="counter-score">35</span>
+                <span className="counter-score">{ props.score }</span>
                 <button className="counter-action increment"> +</button>
-            </div>
+        </div>
+    )
+}
+
+const App = (props) => {
+    return (
+        <div className="scoreboard">
+            <Header 
+                title="Scoreboard" 
+                totalPlayers={1} 
+                />
+
+            {/* Player list */}
+            <Player 
+                name= "Adam"
+                score = {22}
+            />
         </div>
     )
 }
 
 ReactDOM.render(
-    <Player />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 )
