2a44597e33b301904a5629d4a8732b7f12e1088b
asaulters <71015650+asaulters@users.noreply.github.com>
2022-01-13 21:28:58 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+
 const players = [
     {
         name: "Guil",
-        score: 50
+        score: 50,
+        id: 1
       },
       {
         name: "Treasure",
-        score: 85
+        score: 85,
+        id: 2
       },
       {
         name: "Ashley",
-        score: 95
+        score: 95,
+        id: 3
       },
       {
         name: "James",
-        score: 80
+        score: 80,
+        id: 4
       }
 ]
 
@@ -52,14 +57,18 @@ const App = (props) => {
         <div className="scoreboard">
             <Header 
                 title="Scoreboard" 
-                totalPlayers={1} 
+                totalPlayers={props.initialPlayers.length} 
                 />
 
             {/* Player list */}
-            <Player 
-                name= "Adam"
-                score = {22}
-            />
+            {props.initialPlayers.map( player => 
+                <Player 
+                    name= {player.name }
+                    score = {player.score}
+                    key = {player.id.toString()} 
+                />
+            )}
+
         </div>
     )
 }
@@ -67,4 +76,5 @@ const App = (props) => {
 ReactDOM.render(
     <App initialPlayers={players}/>,
     document.getElementById('root')
+
 )
