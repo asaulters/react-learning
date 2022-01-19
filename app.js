7af7e132c77b05ae29a6920e76425ce7ce20e973
asaulters <71015650+asaulters@users.noreply.github.com>
2022-01-18 20:47:23 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-            <button className="remove-player" onClick={ () => props.removePlayer(id)}>✖</button>
+            <button className="remove-player" onClick={ () => props.removePlayer(props.id)}>✖</button>
                 {props.name}
             </span>
             <Counter />
